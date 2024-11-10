7645f451fffd9a8b43699fa3ddfa7ed4a537a6d9
Reza Irfan Wijaya <rezaaagit@gmail.com>
2024-11-10 13:49:04 +0700

    feat: add new env value

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,22 @@
 FROM golang:latest
 
-WORKDIR /APP
+WORKDIR /app
 
 COPY . .
 
 RUN go mod tidy
 
 ARG PORT
-ENV PORT=$PORT
+ARG APP_NAME
+
+ENV PORT=${PORT}
+ENV APP_NAME=${APP_NAME}
+
+RUN echo "PORT: \"$PORT\"" > application.yml
+RUN echo "APP_NAME: \"$APP_NAME\"" >> application.yml
 
 RUN GOOS=linux go build -o go-autoscale
 
-EXPOSE 6767
+EXPOSE ${PORT}
 
 ENTRYPOINT [ "./go-autoscale" ]
--- main.go
+++ main.go
@@ -5,33 +5,39 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/joho/godotenv"
 	"github.com/rezairfanwijaya/go-autoscale.git/handler"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	env, err := getEnv()
-	if err != nil {
-		return
-	}
+	env := getEnv()
 	port := env["PORT"]
+	appName := env["APP_NAME"]
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler.Root)
 	mux.HandleFunc("/users", handler.GetUserList)
 
-	log.Printf("will start the app on port %s\n", port)
+	log.Printf("app %s will start the app on port %s\n", appName, port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), mux); err != nil {
 		log.Fatalf("failed to start the app on port: %s, err: %s\n", port, err)
 	}
 }
 
-func getEnv() (map[string]string, error) {
-	env, err := godotenv.Read("application.yml")
+func getEnv() map[string]string {
+	v := viper.New()
+	v.SetConfigName("application")
+	v.SetConfigType("yaml")
+
+	v.AddConfigPath(".")
+
+	err := v.ReadInConfig()
 	if err != nil {
-		log.Printf("failed to load env file, err: %s\n", err)
-		return env, err
+		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 
-	return env, nil
+	return map[string]string{
+		"PORT":     v.GetString("PORT"),
+		"APP_NAME": v.GetString("APP_NAME"),
+	}
 }
